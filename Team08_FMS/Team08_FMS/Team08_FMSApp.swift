48e75e70cb37ef347044b448e9890b08ecb0c246
Snehil Rai <snehilrai1007@gmail.com>
2025-03-17 18:09:56 +0530

    Adding SwiftUI Xcode Project

---
+++ Team08_FMS/Team08_FMS/Team08_FMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Team08_FMSApp.swift
+//  Team08_FMS
+//
+//  Created by Snehil on 17/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Team08_FMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
