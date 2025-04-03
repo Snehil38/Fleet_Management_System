0da50e5a4b8018a929a0c9060df459dbec62b39b
cangujjar <cangujjar@gmail.com>
2025-04-03 17:01:12 +0530

    final

--- Team08_FMS/Team08_FMS/Driver/Views/DriverTabView.swift
+++ Team08_FMS/Team08_FMS/Driver/Views/DriverTabView.swift
@@ -1170,7 +1170,7 @@ struct UpcomingTripRow: View {
             }
         }
         .padding()
-        .background(Color(.systemBackground))
+        .background(Color(.secondarySystemBackground))
         .cornerRadius(12)
         .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 2)
         .onTapGesture {
